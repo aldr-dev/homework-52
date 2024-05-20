44ef1434c6ee83cd60ce18588641fe3e041a4596
Gordus Alexander <aldrmiller@gmail.com>
2024-05-21 00:32:57 +0600

    реализовал Уровень 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Card from './components/Card/Card.tsx';
 import {useState} from 'react';
 import CardDeck from './lib/CardDeck.ts';
+import PokerHand from './lib/PokerHand.ts';
 
 const App = () => {
   interface IDeck {
@@ -11,16 +12,37 @@ const App = () => {
 
   const [cards, setCards] = useState<IDeck[]>([]);
   const [deck, setDeck] = useState(new CardDeck());
+  const [combination, setCombination] = useState('');
+
+  const resetGame = () => {
+    const cards = new CardDeck();
+    const newDeck = cards.getCards(0);
+    setDeck(cards);
+    setCards(newDeck);
+    setCombination('');
+  };
 
   const dealTheCards = () => {
-    const dealtCards = deck.getCards(5);
+    let dealtCards;
+    if (deck.cards.length < 5) {
+      dealtCards = deck.getCards(2);
+    } else  {
+      dealtCards = deck.getCards(5);
+    }
     setCards(dealtCards);
     setDeck(deck);
+
+    const pokerHand = new PokerHand(dealtCards);
+    const result = pokerHand.getOutcome();
+    setCombination(result);
   };
 
   return (
-    <>
-      <button type="button" onClick={dealTheCards}>Раздать карты</button>
+    <div className="wrapper">
+      <div>Количество кард в колоде: {deck.cards.length}</div>
+      <div>Результат раунда: {combination}</div>
+      <button type="button" onClick={dealTheCards} disabled={deck.cards.length === 0}>Раздать карты</button>
+      <button type="button" onClick={resetGame}>Перезапустить игру</button>
       {cards.length > 0 ? (
         <div className="playingCards faceImages">
           {cards.map((card, index) => (
@@ -28,7 +50,7 @@ const App = () => {
           ))}
         </div>
       ) : null}
-    </>
+    </div>
   );
 };
 
---
+++ src/lib/PokerHand.ts
@@ -0,0 +1,35 @@
+import Card from './Card';
+
+interface RankCounts {
+  [rank: string]: number;
+}
+
+class PokerHand {
+  public cards: Card[];
+
+  constructor(cards: Card[]) {
+    this.cards = cards;
+  }
+
+  getOutcome(): string {
+    const rankCounts: RankCounts = {};
+    let isFlush = true;
+
+    this.cards.forEach((card, index) => {
+      rankCounts[card.rank] = (rankCounts[card.rank] || 0) + 1;
+      if (index > 0 && card.suit !== this.cards[0].suit) isFlush = false;
+    });
+
+    const counts = Object.values(rankCounts);
+    const hasThreeOfAKind = counts.includes(3);
+    const pairCount = counts.filter(count => count === 2).length;
+
+    if (isFlush) return 'Флэш';
+    if (hasThreeOfAKind) return 'Тройка';
+    if (pairCount === 2) return 'Две пары';
+    if (pairCount === 1) return 'Одна пара';
+    return 'Старшая карта';
+  }
+}
+
+export default PokerHand;
