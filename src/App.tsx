8d2167ce7ef9661cfb02b803298b68b11c2ae528
Gordus Alexander <aldrmiller@gmail.com>
2024-05-20 21:54:39 +0600

    реализовал Уровень 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import './App.css';
 import Card from './components/Card/Card.tsx';
+import {useState} from 'react';
+import CardDeck from './lib/CardDeck.ts';
 
 const App = () => {
+  interface IDeck {
+    rank: string;
+    suit: string;
+  }
+
+  const [cards, setCards] = useState<IDeck[]>([]);
+  const [deck, setDeck] = useState(new CardDeck());
+
+  const dealTheCards = () => {
+    const dealtCards = deck.getCards(5);
+    setCards(dealtCards);
+    setDeck(deck);
+  };
+
   return (
-    <div className="playingCards faceImages">
-      <Card/>
-    </div>
+    <>
+      <button type="button" onClick={dealTheCards}>Раздать карты</button>
+      {cards.length > 0 ? (
+        <div className="playingCards faceImages">
+          {cards.map((card, index) => (
+            <Card key={index} suit={card.suit} rank={card.rank}/>
+          ))}
+        </div>
+      ) : null}
+    </>
   );
 };
 
